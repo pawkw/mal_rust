c131dc56b55ea29e22ef2826db39966b519a3814
pawkw <pawkw@users.noreply.github.com>
2022-11-06 02:56:54 -0500

    Added Reader struct and tokenizer.

--- src/reader.rs
+++ src/reader.rs
@@ -1,12 +1,42 @@
 use regex::Regex;
 
-pub fn read_str(input: &String) {
-    let pattern = r#"[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"?|;.*|[^\s\[\]{}('"`,;)]*)"#;
+struct Reader {
+    token_vector: Vec<String>,
+    cursor: usize,
+}
 
-    let re = Regex::new(pattern).unwrap();
+impl Reader {
+    fn new(data: Vec<String>) -> Self {
+        Self {
+            token_vector: data.clone(),
+            cursor: 0,
+        }
+    }
 
-    let tokens: Vec<_> = re.captures_iter(input).collect();
-    for item in tokens {
-        println!("{} ", &item[0].trim());
+    fn peek(&self) -> Option<&String> {
+        self.token_vector.get(self.cursor)
+    }
+
+    fn read(&mut self) -> Option<&String> {
+        self.cursor += 1;
+        self.peek()
     }
 }
+
+pub fn read_str(input: &String) {
+    let token_reader = Reader::new(tokenize(&input));
+    for item in token_reader.token_vector {
+        println!("{}", item);
+    }
+}
+
+fn tokenize(input: &String) -> Vec<String> {
+    const PATTERN: &str = r#"[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"?|;.*|[^\s\[\]{}('"`,;)]*)"#;
+
+    let re = Regex::new(PATTERN).unwrap();
+    re
+        .captures_iter(input)
+        .map(|x| x[0].trim().to_string())
+        .collect()
+}
+
